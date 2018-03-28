3655af5387a6bac3b1277daef5bca56662aa9a6d
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-28 18:56:34 -0500

    Pagination added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import "./styles.css";
 
+import FrontEnd from "./components/FrontEnd";
+import BackEnd from "./components/BackEnd";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      frontend: false,
+      backend: false
+    };
+  }
+
+  manage_frontend_pag = () => {
+    this.setState({
+      frontend: !this.state.frontend,
+      backend: !this.state.backend
+    });
+  };
+
+  manage_backend_pag = () => {
+    this.setState({
+      frontend: !this.state.frontend,
+      backend: !this.state.backend
+    });
+  };
+
   render() {
+    const { frontend, backend } = this.state;
     return (
-      <div>
-        <div className="html_wrap">
-          <label>HTML</label>
-          <div className="bar">
-            <div className="inner_bar" data-percent="80%" />
-          </div>
-        </div>
+      <div className="progress_wrap">
+        <FrontEnd frontend={frontend} />
+        <BackEnd backend={backend} />
+        <ul className="pagination">
+          <li onClick={this.manage_frontend_pag}>1</li>
+          <li onClick={this.manage_backend_pag}>2</li>
+        </ul>
       </div>
     );
   }
