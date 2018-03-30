cbebd61afcb2a4ec9ec6c23a75211a781d88c5f7
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-29 20:40:23 -0500

    additional categories

--- src/components/FrontEnd.js
+++ src/components/FrontEnd.js
@@ -2,25 +2,20 @@ import React, { Component } from "react";
 
 class FrontEnd extends Component {
   render() {
-    const { frontend } = this.props;
     return (
-      <div className={`${frontend ? "initialLoad" : "afterLoad"}`}>
-        <div className="bar_wrap">
-          <label>HTML</label>
+      <div>
+        {" "}<div className="bar_wrap">
+          <label>React</label>
           <div className="bar">
-            <div className="inner_bar html-80" data-percent="80%" />
+            <div className="inner_bar react-80" data-percent="80%" />
           </div>
-        </div>
-        <div className="bar_wrap">
-          <label>CSS</label>
+          <label>GraphQL</label>
           <div className="bar">
-            <div className="inner_bar html-80" data-percent="80%" />
+            <div className="inner_bar graphql-75" data-percent="75%" />
           </div>
-        </div>
-        <div className="bar_wrap">
-          <label>Javascript</label>
+          <label>Redux</label>
           <div className="bar">
-            <div className="inner_bar html-80" data-percent="80%" />
+            <div className="inner_bar redux-50" data-percent="50%" />
           </div>
         </div>
       </div>
