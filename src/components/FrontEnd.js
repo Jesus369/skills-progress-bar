11a2636749e2463bdb619d34e7dbb69cf700ecd7
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-31 12:56:28 -0500

    Update FrontEnd.js

--- src/components/FrontEnd.js
+++ src/components/FrontEnd.js
@@ -4,15 +4,19 @@ class FrontEnd extends Component {
   render() {
     return (
       <div>
-        {" "}<div className="bar_wrap">
+        <div className="bar_wrap">
           <label>React</label>
           <div className="bar">
             <div className="inner_bar react-80" data-percent="80%" />
           </div>
+        </div>
+        <div className="bar_wrap">
           <label>GraphQL</label>
           <div className="bar">
             <div className="inner_bar graphql-75" data-percent="75%" />
           </div>
+        </div>
+        <div className="bar_wrap">
           <label>Redux</label>
           <div className="bar">
             <div className="inner_bar redux-50" data-percent="50%" />
