5d19a6d61afe4e0992199b8255c417746116792e
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-31 12:56:04 -0500

    Update Languages.js

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -4,31 +4,29 @@ class Languages extends Component {
   render() {
     return (
       <div>
-        {" "}<div>
-          <div className="bar_wrap">
-            <label>HTML</label>
-            <div className="bar">
-              <div className="inner_bar html-90" data-percent="90%" />
-            </div>
+        <div className="bar_wrap">
+          <label>HTML</label>
+          <div className="bar">
+            <div className="inner_bar html-90" data-percent="90%" />
           </div>
-          <div className="bar_wrap">
-            <label>Javascript</label>
-            <div className="bar">
-              <div className="inner_bar js-80" data-percent="80%" />
-            </div>
+        </div>
+        <div className="bar_wrap">
+          <label>Javascript</label>
+          <div className="bar">
+            <div className="inner_bar js-80" data-percent="80%" />
           </div>
-          <div className="bar_wrap">
-            <label>CSS</label>
-            <div className="bar">
-              <div className="inner_bar css-70" data-percent="70%" />
-            </div>
+        </div>
+        <div className="bar_wrap">
+          <label>CSS</label>
+          <div className="bar">
+            <div className="inner_bar css-70" data-percent="70%" />
           </div>
-          <div className="bar_wrap">
-            <label>Python</label>
-            <div className="bar">
-              <div className="inner_bar python-40" data-percent="40%" />
-            </div>
-          </div>{" "}
+        </div>
+        <div className="bar_wrap">
+          <label>Python</label>
+          <div className="bar">
+            <div className="inner_bar python-40" data-percent="40%" />
+          </div>
         </div>
       </div>
     );
